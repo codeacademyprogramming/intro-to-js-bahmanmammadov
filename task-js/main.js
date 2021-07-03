076f72131ef388fa5d35d2ef65b9bb0880913566
bahmanmammadov <bahmanshm@code.edu.az>
2021-07-03 17:31:55 +0400

    tapsiriq

---
+++ task-js/main.js
@@ -0,0 +1,10 @@
+let Name = prompt("Adinizi yazin")
+let SurName = prompt("Soyadinizi yazin") 
+let Age = prompt("Yasinizi yazin") 
+
+if(Age>18){
+    console.log("sehifeye daxil ola bilersiniz")
+}
+else{
+    console.log("sehifeye daxil ola bilmersiz")
+}
